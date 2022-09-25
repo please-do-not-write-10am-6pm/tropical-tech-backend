9340d96e82f9e7edc6a7fd5243316f48df69f802
please-do-not-write-10am-6pm <nothingpermanent0304@gmail.com>
2022-09-24 23:40:29 -0400

    changed: request limit of search result

--- src/controllers/hotel.controller.mjs
+++ src/controllers/hotel.controller.mjs
@@ -75,7 +75,7 @@ export const getAll = async (req, res) => {
   const end = { latitude: Number(geolocation.latitude), longitude: Number(geolocation.longitude) }
 
   const filter = {
-    maxHotels: 12
+    maxHotels: 9
   }
   query.filter = filter
 
